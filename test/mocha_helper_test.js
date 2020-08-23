0574de423f018bddf77b2fd5a581eb1dd50d4d1a
poorvasha <poorvashadmp@gmail.com>
2020-08-23 21:46:04 +0530

    [IMP] TestModule : Database connection established.

--- test/mocha_helper_test.js
+++ test/mocha_helper_test.js
@@ -1,1 +1,15 @@
-// 
+// importing module "mongoose"
+const mongoose = require("mongoose");
+
+// Connecting to mongoDB
+mongoose.connect("mongodb://127.0.0.1:27017", {useNewUrlParser : true})
+
+// checking connection
+mongoose.connection
+    // eventEmitters, .once-> calls at once (intialization)
+    .once("open", () => console.log("connected"))
+    
+    // .on -> full listner
+    .on("error", error => {
+        console.log("Error Name", error);
+    });
